376f35f01ad565e3e0f386cfaeb9819bbde955a1
andrey019 <andrey019@gmail.com>
2016-04-08 16:49:27 +0300

    testing

---
+++ src/TaskTest2/test1.java
@@ -0,0 +1,10 @@
+package TaskTest2;
+
+/**
+ * Created by andrey on 08.04.16.
+ */
+public class test1 {
+    public static void main(String[] args){
+        System.out.println("ololo!");
+    }
+}
